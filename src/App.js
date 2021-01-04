b4db7dcfae65894c29bce9f807553560a57599a5
Ken Tiet <ktiet@escautomation.com>
2021-01-03 22:41:34 -0800

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Rank from './components/Rank/Rank'
+import SignIn from './components/SignIn/SignIn'
 import Particles from 'react-particles-js'
 import React, { Component } from 'react';
 import Clarifai from 'clarifai'
 
 const app = new Clarifai.App({
-  // apiKey: 'a130d7cd2b35429ea59c17e5ebe6f778'
-  apiKey: 'YOUR API KEY'
+  apiKey: 'a130d7cd2b35429ea59c17e5ebe6f778'
+  // apiKey: 'YOUR API KEY'
  });
 
 const particlesOptions = {
@@ -30,12 +31,14 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imgUrl: ''
+      imgUrl: '',
+      box: {},
+      route: 'signin'
     }
   }
 
   calculateFaceLocation = (data) => {
-      const clarifaiFace = data.outputs[0].data.regions[0].regions_info.bounding_box;
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
       const img = document.getElementById('inputImage');
       const width = Number(img.width);
       const height = Number(img.height);
@@ -44,8 +47,13 @@ class App extends Component {
       return {
         leftCol: clarifaiFace.left_col * width,
         topRow: clarifaiFace.top_row * height,
-        rightCol: width
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
       }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -56,8 +64,12 @@ class App extends Component {
     this.setState({imgUrl: this.state.input})
     // console.log('click')
     app.models.predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
-    .then(res => this.calculateFaceLocation(res))
+    .then(res => this.displayFaceBox(this.calculateFaceLocation(res)))
     .catch(err => console.log(err))
+  }
+
+  onRouteChange = () => {
+    this.setState({route:'home'})
   }
 
   render() {
@@ -67,13 +79,17 @@ class App extends Component {
           className='particles'
           params={particlesOptions} />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit = {this.onButtonSubmit}
-        />
-        <FaceRecognition imgUrl={this.state.imgUrl}/>
+        { this.state.route === 'signin' 
+          ? <SignIn onRouteChange={this.onRouteChange}/> 
+          : <div> <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit = {this.onButtonSubmit}
+          />
+          <FaceRecognition box={this.state.box} imgUrl={this.state.imgUrl}/>
+          </div>
+        }
       </div>
     );
   }
