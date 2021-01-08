14f310337a1b8ddc0968c889fad000898c46dc34
Ken Tiet <ktiet@escautomation.com>
2021-01-07 22:21:23 -0800

    extend api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 
 const database = {
@@ -39,6 +41,7 @@ app.post('/signin', (req,res)=>{
 })
 
 app.post('/register', (req,res)=>{
+    console.log(req)
     const {email, name, password} = req.body
     database.users.push({
         id: '125',
@@ -50,6 +53,34 @@ app.post('/register', (req,res)=>{
     })
     res.json(database.users[database.users.length-1])
 })
+
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id == id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found) res.status(404).json('no such user')
+})
+
+// app.post('/image', (res, req) => {
+//     console.log(req.body)
+//     const { id } = req.body;
+//     let found = false;
+//     database.users.forEach(user => {
+//         if (user.id === id)
+//         {
+//             found = true;
+//             user.entries++;
+//             return res.json(user.entries);
+//         }
+//     })
+//     if(!found) res.status(404).json('no such user')
+// })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000')
